5a2d7a8c30e58711b05ff6547b144e0e4c9ac38a
zdodev <iver0945@gmail.com>
2020-11-11 01:27:20 +0900

    사용자 경고 기능 함수 추가

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -6,14 +6,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+
         loadWebPage(url: "https://www.rapid7.com")
     }
     
     func loadWebPage(url: String) {
-        if let requestUrl = URL(string: url) {
-            let request = URLRequest(url: requestUrl)
-            webView.load(request)
+        guard let requestUrl = URL(string: url) else {
+            showInvaildUrlAlert()
+            return
         }
+        
+        let request = URLRequest(url: requestUrl)
+        webView.load(request)
+    }
+    
+    func showInvaildUrlAlert() {
+        let alert = UIAlertController(title: "접근할 수 없습니다.", message: "유효하지 않은 URL입니다.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        self.present(alert, animated: true)
     }
 }
