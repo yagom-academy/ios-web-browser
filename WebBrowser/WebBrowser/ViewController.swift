397339cf453b1a0d89ddd8d58b22c237a416aa47
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-10 23:01:23 +0900

    searchBar에 주소를 입력 후 검색버튼을 누르면 이동 - 4

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -12,15 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet var webView: WebView!
     @IBOutlet var toolBar: ToolBar!
     
+    //Strong일땐 naver가 안들어 가지는데 weak일대는 naver가 들어가진다?
+    @IBOutlet weak var searchBar: SearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Configuration views
         webView.config()
         toolBar.config()
+        searchBar.config()
         
         //ToolBarDelegate
         toolBar.toolBarDelegate = self
+    }
+    
+    @IBAction func didTapSearchButton(_ sender: UIButton) {
+        guard let urlToRequest = searchBar.text else {fatalError("URL이 유효하지 않습니다!")}
+        print(urlToRequest)
+        webView.urlRequest(to: urlToRequest)
     }
 }
 
