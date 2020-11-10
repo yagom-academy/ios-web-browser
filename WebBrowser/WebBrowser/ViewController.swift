8dfd34fb0d70382b1261435ded1ee100dcec9304
wonhee <wonhee010@gmail.com>
2020-11-10 16:00:14 +0900

    1. add setWebView func

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -5,14 +5,35 @@
 // 
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    private let myAddress = "https://www.indiepost.co.kr"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setWebView()
     }
-
-
+    
+    func setWebView() {
+        guard let url = URL(string: myAddress) else {
+            return makeErrorAlert()
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    func makeErrorAlert() {
+        let alert = UIAlertController(title: "오류", message: "url을 불러오는데 오류가 있습니다.", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(confirmAction)
+        present(alert, animated: false, completion: nil)
+    }
 }
 
