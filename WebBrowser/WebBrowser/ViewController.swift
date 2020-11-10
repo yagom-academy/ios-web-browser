4ada8ab7a820a9a16f3405c795280d3ce60b2fa8
HakJu <gkrwnrh@gmail.com>
2020-11-10 22:11:26 +0900

    fix(step 1 / 그린, 꼬말): 코드 리뷰 후 수정

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -6,23 +6,16 @@ import WebKit
 class ViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
-    override func loadView() {
-        super.loadView()
-        
-        webView? = WKWebView(frame: self.view.frame)
-        self.view = self.webView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         loadUrl()
     }
-
-    /*
-     웹 페이지를 보여주는 함수
-     code by 그린,꼬말
-    */
+    
     func loadUrl() {
         guard let url = URL(string: "https://yagom.net") else { return }
         let request = URLRequest(url: url)
         
-        webView?.load(request)
+        webView.load(request)
     }
 }
