d8ce5423d20200d973639bc9d362cb3caa4ac119
zziro95 <opers7878@gmail.com>
2020-11-10 19:10:12 +0900

    add(Main.storyboard,ViewController.swift 0.1 / jake,zziro): 웹뷰를 로드시켜주는 함수 구현

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -5,14 +5,27 @@
 // 
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var webView: WKWebView!
+    private let myURL: URL? = URL(string:"https://www.naver.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebView(URL: myURL)
     }
-
+    
+    /*
+    우리조가 즐켜찾는 웹페이지 URL로 웹뷰를 로드시켜주는 함수
+    code by jake,zziro
+    */
+    func loadWebView(URL: URL?) {
+        guard let myURL = URL else { print("URL주소가 없어서 종료됩니다."); return }
+        let myRequest = URLRequest(url: myURL)
+        webView.load(myRequest)
+    }
 
 }
 
