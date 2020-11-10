5ae659e0d918df76497ee0f86eb3b7fa8b012716
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-10 21:39:05 +0900

    toolBar에서 Back,Forward,Reload버튼을 추가하여 기능 부여 - 2

--- WebBrowser/WebBrowser/ToolBar.swift
+++ WebBrowser/WebBrowser/ToolBar.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
+protocol ToolBarDelegate: AnyObject {
+    func didTapBackButton()
+    func didTapForwardButton()
+    func didTapReloadButton()
+}
+
 class ToolBar: UIToolbar {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var toolBarDelegate: ToolBarDelegate?
+    
+    public func config() {
+        
     }
-    */
-
+    
+    //MARK: - Actions
+    @IBAction func didTapBackButton(_ sender: UIBarButtonItem) {
+        toolBarDelegate?.didTapBackButton()
+    }
+    @IBAction func didTapForwardButton(_ sender: UIBarButtonItem) {
+        toolBarDelegate?.didTapForwardButton()
+    }
+    @IBAction func didTapReloadButton(_ sender: UIBarButtonItem) {
+        toolBarDelegate?.didTapReloadButton()
+    }
 }
--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -9,8 +9,31 @@ import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: WebView!
+    @IBOutlet var toolBar: ToolBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Configuration views
+        webView.config()
+        toolBar.config()
+        
+        //ToolBarDelegate
+        toolBar.toolBarDelegate = self
     }
 }
 
+extension ViewController: ToolBarDelegate {
+    func didTapBackButton() {
+        webView.goBack()
+    }
+    
+    func didTapForwardButton() {
+        webView.goForward()
+    }
+    
+    func didTapReloadButton() {
+        webView.reload()
+    }
+}
