ba2e561c1bf13674dc1a885f35ef64d9b82bd823
GREENOVER <humains29@gmail.com>
2020-11-10 19:19:29 +0900

    add(toolBar / 그린): 툴바 구성

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -6,23 +6,49 @@ import WebKit
 class ViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
-    override func loadView() {
-        super.loadView()
-        
-        webView? = WKWebView(frame: self.view.frame)
-        self.view = self.webView!
-        
-        loadUrl()
-    }
+//    override func loadView() {
+//        super.loadView()
+//
+//        webView? = WKWebView(frame: self.view.frame)
+//        view = webView!
+//
+//    }
 
-    /*
-     웹 페이지를 보여주는 함수
-     code by 그린,꼬말
-    */
     func loadUrl() {
         guard let url = URL(string: "https://yagom.net") else { return }
         let request = URLRequest(url: url)
+
+        webView.load(request)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view = webView!
         
-        webView?.load(request)
+        loadUrl()
+    }
+    
+    
+    @IBOutlet weak var btnGoForward: UIBarButtonItem!
+    @IBOutlet weak var btnReload: UIBarButtonItem!
+    @IBOutlet weak var btnGoBack: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+        webView.goForward()
+        }
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+        webView.goBack()
+        }
     }
 }
+
+
