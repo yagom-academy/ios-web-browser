ecab725e348e295c74d6fac5b1228ecfef693c01
taetae <taetae.dev@gmail.com>
2020-11-11 00:55:53 +0900

    Feat: 툴바 기능 구현

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -10,16 +10,40 @@ import WebKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var webVeiw: WKWebView!
+    @IBOutlet weak var goBackButton: UIBarButtonItem!
+    @IBOutlet weak var goForwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadPageButton: UIBarButtonItem!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         guard let initialURL = URL(string: "https://m.naver.com") else {
                     print("URL is nil : 잘못된 값이 입력되었습니다.")
                     return
-    }
+        }
+        
         let urlRequest = URLRequest(url: initialURL)
         webVeiw.load(urlRequest)
 
+    }
+    
+    @IBAction func goBack() {
+        
+        if webVeiw.canGoBack {
+            webVeiw.goBack()
+        }
+    }
+    
+    @IBAction func goForward() {
+        
+        if webVeiw.canGoForward {
+            webVeiw.goForward()
+        }
+    }
+    
+    @IBAction func reloadPage() {
+        
+        webVeiw.reload()
     }
 }
