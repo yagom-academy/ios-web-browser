5e67aa67e06a2c38c994912ff2176bc2dad3ff14
HakJu <gkrwnrh@gmail.com>
2020-11-10 22:02:22 +0900

    add(touchUpChangeButton / kkomal): 버튼 클릭 이벤트 미완

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -5,24 +5,83 @@ import WebKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var buttonGoBack: UIBarButtonItem!
+    @IBOutlet weak var buttonReload: UIBarButtonItem!
+    @IBOutlet weak var buttonGoForward: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var inputText: UITextField!
     
-    override func loadView() {
-        super.loadView()
-        
-        webView? = WKWebView(frame: self.view.frame)
-        self.view = self.webView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         loadUrl()
+//        changeUrl(input: <#T##String#>)
     }
-
-    /*
-     웹 페이지를 보여주는 함수
-     code by 그린,꼬말
-    */
+    
     func loadUrl() {
         guard let url = URL(string: "https://yagom.net") else { return }
         let request = URLRequest(url: url)
-        
-        webView?.load(request)
+
+        webView.load(request)
     }
+    
+    @IBAction func buttonGoBack(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+
+    @IBAction func buttonReload(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
+    @IBAction func buttonGoForward(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    func changeUrl(input : String) {
+        guard let inputUrl = URL(string: input) else { return }
+        let changeRequest = URLRequest(url: inputUrl)
+        
+        webView?.load(changeRequest)
+    }
+    
+    @IBAction func touchUpChangeButton(_ sender: UIButton) {
+        print(inputText.text)
+    }
+
+    @IBAction func inputTextValue(_ sender: UITextField) {
+        print(inputText.text)
+    }
+    
 }
+
+/*
+ class ViewController: UIViewController {
+  
+     @IBOutlet weak var inputtext: UITextField!
+     
+     @IBOutlet weak var outputtext: UILabel!
+     
+     @IBAction func insert(sender: AnyObject) {
+         outputtext.text=inputtext.text;
+         inputtext.text="";
+         
+     }
+     @IBAction func clear(sender: AnyObject) {
+         outputtext.text="";
+         
+     }
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view, typically from a nib.
+     }
+  
+     override func didReceiveMemoryWarning() {
+         super.didReceiveMemoryWarning()
+         // Dispose of any resources that can be recreated.
+     }
+ }
+ */
