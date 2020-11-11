d6f8c45350cdf079542f8c24433197db00177545
yeon <jryoun1@naver.com>
2020-11-11 11:42:37 +0900

    fix(ViewController.swift 0.1 / zziro,jake) : 코드리뷰 후 수정

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -10,20 +10,20 @@ import WebKit
 class ViewController: UIViewController {
     
     @IBOutlet var webView: WKWebView!
-    private let myURL: URL? = URL(string: "https://www.github.com")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadWebView(URL: myURL)
+        loadWebPageToWebView()
     }
     
     /*
     우리조가 즐켜찾는 웹페이지 URL로 웹뷰를 로드시켜주는 함수
     code by jake,zziro
     */
-    func loadWebView(URL: URL?) {
-        guard let myURL = URL else { print("URL주소가 없어서 종료됩니다."); return }
-        let myRequest = URLRequest(url: myURL)
+    func loadWebPageToWebView() {
+        let favoriteURL: URL? = URL(string: "https://www.github.com")
+        guard let URL = favoriteURL else { print("URL주소가 없어서 종료됩니다."); return }
+        let myRequest = URLRequest(url: URL)
         webView.load(myRequest)
     }
 
