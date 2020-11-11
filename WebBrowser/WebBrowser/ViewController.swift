ee60490bbff24f62275a985087bc129c0fcbaf28
wonhee <wonhee010@gmail.com>
2020-11-11 15:59:35 +0900

    5. add errormessage enum

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var moveButton: UIButton!
-    @IBOutlet weak var backButton: UIBarButtonItem!
-    @IBOutlet weak var forwarkButton: UIBarButtonItem!
-    @IBOutlet weak var reloadButton: UIBarButtonItem!
     
-    private let myAddress = "https://www.indiepost.co.kr"
+    enum ErrorMessage : String {
+        case convertUrl = "url로 변환하는데 문제가 있습니다."
+        case moveForward = "앞으로 이동할 페이지가 없습니다."
+        case moveBack = "뒤로 이동할 페이지가 없습니다."
+        case loadPage = "페이지를 새로고침하는데 실패했습니다."
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,19 +26,47 @@ class ViewController: UIViewController {
     }
     
     func setWebView() {
-        guard let url = URL(string: myAddress) else {
-            return showErrorAlert()
+        
+        let startUrl = "https://www.indiepost.co.kr"
+        guard let url = URL(string: startUrl) else {
+            return showErrorAlert(error: .convertUrl)
         }
         
         let request = URLRequest(url: url)
         webView.load(request)
     }
     
-    func showErrorAlert() {
-        let alert = UIAlertController(title: "오류", message: "url을 불러오는데 오류가 있습니다.", preferredStyle: .alert)
+    func showErrorAlert(error: ErrorMessage) {
+        let alert = UIAlertController(title: nil, message: error.rawValue, preferredStyle: .alert)
         let confirmAction = UIAlertAction(title: "확인", style: .default, handler: nil)
         alert.addAction(confirmAction)
         present(alert, animated: false, completion: nil)
+    }
+    
+    @IBAction func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+        else {
+            showErrorAlert(error: .moveBack)
+        }
+    }
+    
+    @IBAction func goForward() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+        else {
+            showErrorAlert(error: .moveForward)
+        }
+    }
+    
+    @IBAction func reloadPage() {
+        
+    }
+    
+    @IBAction func movePage() {
+        
     }
 }
 
