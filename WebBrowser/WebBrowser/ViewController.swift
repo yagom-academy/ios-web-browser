9be2cf9d6233af2528fe637a2284dac6bb44f3d8
jae57 <ggapsong@naver.com>
2020-11-11 13:04:38 +0900

    ADD: webview

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -5,14 +5,17 @@
 // 
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let myURL = URL(string: "https://www.apple.com") else { return }
+        let myRequest = URLRequest(url: myURL)
+        webView.load(myRequest)
     }
-
-
 }
 
