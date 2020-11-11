66896f876aab0b692a368664455da98901c49084
SM <hcooch2ch3@gmail.com>
2020-11-11 23:59:33 +0900

    [#3] Step2 구현 완료

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -10,17 +10,45 @@ import WebKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mainWebView: WKWebView!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var backBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var forwardBarButtonItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /// 웹뷰에 구글 페이지 로드
-        let urlString = "https://www.google.com"
-        if let url = URL(string: urlString) {
-            let urlReq = URLRequest(url: url)
-            mainWebView.load(urlReq)
+        self.addressTextField.text = "https://www.google.com"
+        self.loadWebPage(of: "https://www.google.com")
+    }
+    
+    /// 웹뷰에 입력한 주소페이지를 로드하는 함수.
+    ///
+    /// - Parameter of: 이동할 웹페이지의 주소 문자열.
+    func loadWebPage(of address: String) {
+        if let url = URL(string: address) {
+            let urlRequest = URLRequest(url: url)
+            self.mainWebView.load(urlRequest)
         }
-       
+    }
+    
+    @IBAction func touchUpGoBarButtonItme(_ sender: UIBarButtonItem) {
+        if let address = addressTextField.text {
+            loadWebPage(of: address)
+            
+            self.addressTextField.endEditing(true)
+        }
+    }
+    
+    @IBAction func touchUpBackBarButtonItme(_ sender: UIBarButtonItem) {
+        mainWebView.goBack()
+    }
+    
+    @IBAction func touchUpForwardBarButtonItme(_ sender: UIBarButtonItem) {
+        mainWebView.goForward()
+    }
+    
+    @IBAction func touchUpRefreshBarButtonItem(_ sender: UIBarButtonItem) {
+        mainWebView.reload()
     }
 
 }
