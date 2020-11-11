200edb11f8e72a94b229df70264af7b460641915
yeon <jryoun1@naver.com>
2020-11-12 00:17:04 +0900

    add(ViewController.swift, Main.storyboard 0.2 / zziro,jake) : step2까지 구현

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -9,22 +9,47 @@ import WebKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var webView: WKWebView!
-    private let myURL: URL? = URL(string: "https://www.github.com")
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var urlEnteredTextField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadWebView(URL: myURL)
+        let favoriteURL: String? = "https://www.github.com"
+        loadWebPageToWebView(to: favoriteURL)
     }
     
-    /*
-    우리조가 즐켜찾는 웹페이지 URL로 웹뷰를 로드시켜주는 함수
-    code by jake,zziro
-    */
-    func loadWebView(URL: URL?) {
-        guard let myURL = URL else { print("URL주소가 없어서 종료됩니다."); return }
-        let myRequest = URLRequest(url: myURL)
-        webView.load(myRequest)
+    func convertStringToURL(input url: String?) -> URL? {
+        guard let stringURL = url else { print("입력 URL이 없어서 종료됩니다."); return nil }
+        let convertedURL: URL? = URL(string: stringURL)
+        return convertedURL
+    }
+    
+    func loadWebPageToWebView(to url: String?) {
+        guard let requestedURL = convertStringToURL(input: url) else { print("변환된 URL이 존재하지 않습니다."); return }
+        let request = URLRequest(url: requestedURL)
+        webView.load(request)
+    }
+    
+    @IBAction func goToEnteredURL(_ sender: UIButton) {
+        guard let enteredURL = urlEnteredTextField?.text else { print("주소창에 입력된 주소가 없습니다."); return }
+        loadWebPageToWebView(to: enteredURL)
+    }
+    
+    @IBAction func goBack(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func goForward(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func reload(_ sender: UIBarButtonItem) {
+        webView.reload()
     }
 
 }
