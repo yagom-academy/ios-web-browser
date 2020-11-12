d62ccd292812a2cfd41831dc49f238b5f8555377
yeon <jryoun1@naver.com>
2020-11-12 12:30:38 +0900

    add(ViewController.swift 0.3 /jake) : Alert 보여주는 함수

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     func convertStringToURL(input string: String?) -> URL? {
         guard let urlString = string else {
-            print("입력 URL이 없어서 종료됩니다.")
+            showAlert(message: "입력 URL이 없어서 종료됩니다.")
             return nil
         }
         let convertedURL: URL? = URL(string: urlString)
@@ -30,16 +30,25 @@ class ViewController: UIViewController {
     
     private func loadWebPageToWebView(to string: String?) {
         guard let requestedURL = convertStringToURL(input: string) else {
-            print("변환된 URL이 존재하지 않습니다.")
+            showAlert(message: "변환할 URL이 존재하지 않습니다.")
             return
         }
         let request = URLRequest(url: requestedURL)
         webView.load(request)
     }
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func goToEnteredURL(_ sender: UIButton) {
         guard let enteredURL = urlEnteredTextField?.text else {
-            print("주소창에 입력된 주소가 없습니다.")
+            showAlert(message: "주소창에 입력된 주소가 없습니다.")
             return
         }
         loadWebPageToWebView(to: enteredURL)
