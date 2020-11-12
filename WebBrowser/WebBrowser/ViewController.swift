feaccb1dc402045e874879239d82d4203cb2e624
innieminnie <rkddlsl1001@gmail.com>
2020-11-12 10:35:25 +0900

    refactor: toolbar button 활성화/비활성화 delgate 방식으로 교체

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -19,23 +19,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBarURL.delegate = self
-        invalidURLLabel.isHidden = true
         
-        guard let initialURL = URL(string: "https://m.naver.com") else {
+        searchBarURL.delegate = self
+        webView.navigationDelegate = self
+        invalidURLLabel.isHidden = true
+        goBackButton.isEnabled = webView.canGoBack
+        goForwardButton.isEnabled = webView.canGoBack
+        
+        let initialURLString = "https://m.naver.com"
+        
+        guard let initialURL = convertToURL(of: initialURLString) else {
             print("URL is nil : 잘못된 값이 입력되었습니다.")
-            invalidURLLabel.text = "잘못된 URL값 입니다."
-            invalidURLLabel.isHidden = false
+            let notFoundURLAlert = UIAlertController(title: "Not Found", message: "입력한 주소가 올바른 형태가 아닙니다.", preferredStyle: .alert)
+            let exitAction = UIAlertAction(title: "닫기", style: .default, handler: nil)
+            notFoundURLAlert.addAction(exitAction)
+            present(notFoundURLAlert,animated: false,completion: nil)
             return
         }
-        
+        //URL Request Check Function
         loadWebView(of: initialURL)
-        
-        goBackButton.isEnabled = webView.canGoBack
-        goForwardButton.isEnabled = webView.canGoForward
-        
-        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
-        
     }
     
     @IBAction func goBack() {
@@ -55,19 +57,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tappedMoveToURLButton(_ sender: UIButton) {
-        guard let requestedURL = convertToURL(of: searchBarURL.text) else {
-            print("URL is nil : 잘못된 값이 입력되었습니다.")
+        guard let searchBarURLText = searchBarURL.text else {
             return
         }
+        
+        guard let requestedURL = convertToURL(of: searchBarURLText) else {
+            print("URL is nil : 잘못된 값이 입력되었습니다.")
+            let notFoundURLAlert = UIAlertController(title: "Not Found", message: "입력한 주소가 올바른 형태가 아닙니다.", preferredStyle: .alert)
+            let exitAction = UIAlertAction(title: "닫기", style: .default, handler: nil)
+            notFoundURLAlert.addAction(exitAction)
+            present(notFoundURLAlert,animated: false,completion: nil)
+            return
+        }
+        
         loadWebView(of: requestedURL)
     }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if (keyPath == "loading") {
-            goBackButton.isEnabled = webView.canGoBack
-            goForwardButton.isEnabled = webView.canGoForward
-            showCurrentAddress()
-        }
+}
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        goBackButton.isEnabled = webView.canGoBack
+        goForwardButton.isEnabled = webView.canGoForward
+        showCurrentAddress()
     }
 }
 extension ViewController: UISearchBarDelegate {
@@ -85,22 +95,20 @@ extension ViewController {
     private func loadWebView(of requestedURL: URL) {
         let urlRequest = URLRequest(url: requestedURL)
         searchBarURL.text = requestedURL.absoluteString
+        print(urlRequest)
         webView.load(urlRequest)
     }
     
     private func convertToURL(of searchBarText: String?) -> URL? {
-        guard let requestedURLText = searchBarText else {
+        guard let requestedURLText = searchBarText, let testUrl = URL(string: requestedURLText) else {
+            print("URL is nil : 잘못된 값이 입력되었습니다.")
+            invalidURLLabel.text = "잘못된 URL값 입니다."
+            invalidURLLabel.isHidden = false
             return nil
         }
-        
-        if requestedURLText.hasPrefix("https://") || requestedURLText.hasPrefix("http://") {
-            return URL(string: requestedURLText)
-        }else{
-            return URL(string:"https://" + requestedURLText)
-            
-        }
+        return testUrl
     }
-    
+
     private func showCurrentAddress() {
         let currentAddress = webView.url
         searchBarURL.text = currentAddress?.absoluteString
