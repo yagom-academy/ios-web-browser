8534032cb57f3c3441018f31cb97c38e99455ad7
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-13 02:23:53 +0900

    리뷰와 코드 수정 - 5

--- WebBrowser/WebBrowser/SearchBar.swift
+++ WebBrowser/WebBrowser/SearchBar.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
+//A specialized view for receiving search-related information from the user.
+//유저에게서 검색과 관련된 정보를 받는 것에 특화된 View이므로 UISearchBar를 사용.
 class SearchBar: UISearchBar {
-
-    public func config() {
+    func searchBarConfig() {
         //searchbar의 왼쪽 돋보기 icon 없애기!
         self.setImage(UIImage(), for: .search, state: .normal)
         self.autocapitalizationType = .none
--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -12,38 +12,38 @@ class ViewController: UIViewController {
     @IBOutlet var webView: WebView!
     @IBOutlet var toolBar: ToolBar!
     
-    //Strong일땐 naver가 안들어 가지는데 weak일대는 naver가 들어가진다?
-    @IBOutlet weak var searchBar: SearchBar!
+    //Strong일땐 naver가 안들어 가지는데 weak일대는 naver가 들어가진다? -> 잘들어가지네요!
+    @IBOutlet var searchBar: SearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.urlRequest(to: "https://www.apple.com")
         
         //Configuration views
-        webView.config()
-        toolBar.config()
-        searchBar.config()
+        searchBar.searchBarConfig()
         
         //ToolBarDelegate
         toolBar.toolBarDelegate = self
     }
     
     @IBAction func didTapSearchButton(_ sender: UIButton) {
-        guard let urlToRequest = searchBar.text else {fatalError("URL이 유효하지 않습니다!")}
-        print(urlToRequest)
+        guard let urlToRequest = searchBar.text else {
+            fatalError("URL이 유효하지 않습니다!")
+        }
         webView.urlRequest(to: urlToRequest)
     }
 }
 
 extension ViewController: ToolBarDelegate {
-    func didTapBackButton() {
+    func didTapBackButton(toolBar: ToolBar) {
         webView.goBack()
     }
     
-    func didTapForwardButton() {
+    func didTapForwardButton(toolBar: ToolBar) {
         webView.goForward()
     }
     
-    func didTapReloadButton() {
+    func didTapReloadButton(toolBar: ToolBar) {
         webView.reload()
     }
 }
