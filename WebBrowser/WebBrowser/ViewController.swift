e0504f7a52e1e53c634893528bfe080f2ea0d7b0
GREENOVER <humains29@gmail.com>
2020-11-12 12:27:16 +0900

    fix(Step3 그린,꼬말): 인디케이터 추가 및 로직에 따른 함수 순서 변경

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -3,45 +3,36 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var webView: WKWebView!
-    @IBOutlet weak var buttonGoBack: UIBarButtonItem!
-    @IBOutlet weak var buttonReload: UIBarButtonItem!
-    @IBOutlet weak var buttonGoForward: UIBarButtonItem!
-    @IBOutlet weak var toolBar: UIToolbar!
-    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var loadIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadUrl()
     }
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        loadIndicator.startAnimating()
+    }
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadIndicator.stopAnimating()
+    }
+
     func loadUrl() {
         guard let url: URL = URL(string: "https://yagom.net") else { return }
         let request: URLRequest = URLRequest(url: url)
-
+        
+        webView.navigationDelegate = self
+        
         webView.load(request)
     }
     
-    @IBAction func buttonGoBack(_ sender: UIBarButtonItem) {
-        if webView.canGoBack {
-            webView.goBack()
-        }
-    }
-
-    @IBAction func buttonReload(_ sender: UIBarButtonItem) {
-        webView.reload()
-    }
-    
-    @IBAction func buttonGoForward(_ sender: UIBarButtonItem) {
-        if webView.canGoForward {
-            webView.goForward()
-        }
-    }
-    
     @IBAction func touchUpChangeButton(_ sender: UIButton) {
-        let inputTextUrl: String? = inputText.text
+        let inputTextUrl: String? = inputField.text
         guard let inputUrl: String = inputTextUrl else { return }
 
         changeUrl(inputUrl)
@@ -49,15 +40,31 @@ class ViewController: UIViewController {
     
     func changeUrl(_ inputUrl: String) {
         let identifiedUrl: String = verifyUrl(inputUrl)
-        
-        guard let changeUrl: URL = URL(string: identifiedUrl) else { return }
-        let changeRequest: URLRequest = URLRequest(url: changeUrl)
+
+        guard let urlToChange: URL = URL(string: identifiedUrl) else { return }
+        let changeRequest: URLRequest = URLRequest(url: urlToChange)
         
         webView.load(changeRequest)
     }
     
+    @IBAction func touchUpBackButton(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+
+    @IBAction func touchUpReloadButton(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
+    @IBAction func touchUpForwardButton(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
     func verifyUrl(_ unidentifiedUrl: String) -> String {
-        let identifyUrl = unidentifiedUrl
+        let identifyUrl = unidentifiedUrl.lowercased()
         if !(identifyUrl.hasPrefix("http://") || identifyUrl.hasPrefix("https://")) {
             showAlertMessage()
         }
@@ -66,7 +73,7 @@ class ViewController: UIViewController {
     
     func showAlertMessage() {
         let alert = UIAlertController(title: nil, message: "입력한 주소가 올바른 형태가 아닙니다.", preferredStyle: UIAlertController.Style.alert)
-        let okAction = UIAlertAction(title: "확인", style: .cancel, handler : nil )
+        let okAction = UIAlertAction(title: "확인", style: .cancel, handler : nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
