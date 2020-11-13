2f289f3b870b9c035dd4bb9f46f0022824105267
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-13 17:09:04 +0900

    주소 유효성 검사 코드 - 6

--- WebBrowser/WebBrowser/WebView.swift
+++ WebBrowser/WebBrowser/WebView.swift
@@ -9,10 +9,31 @@ import UIKit
 import WebKit
 
 class WebView: WKWebView {
-    func urlRequest(to url: String) {
-        let myURL = URL(string: url)
-        guard let urlToRequest = myURL else {fatalError("URL이 유효하지 않습니다!")}
-        let createRequest = URLRequest(url: urlToRequest)
-        self.load(createRequest)
+    func urlRequest(to url: String) -> Bool{
+        //Checking whether URL is Validate
+        if checkURLValidate(url) {
+            //Validate URL
+            let myURL = URL(string: url)
+            guard let urlToRequest = myURL else {fatalError("URL이 유효하지 않습니다!")}
+            let createRequest = URLRequest(url: urlToRequest)
+            self.load(createRequest)
+            return true
+        } else {
+            //Invalidate URL
+            return false
+        }
+    }
+    private func checkURLValidate(_ string: String?) -> Bool {
+        guard let unwrappingString = string,
+              let url = URL(string: unwrappingString)
+        else {return false}
+        
+        //url을 open할 수 없다면 false를 Return
+        if !UIApplication.shared.canOpenURL(url) { return false }
+        
+        //정규표현식을 사용한 URL의 유효성 검사
+        let regularExpression = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regularExpression])
+        return predicate.evaluate(with: string)
     }
 }
