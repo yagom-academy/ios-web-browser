98c417fe93e21e3146d6ff522c91b2a9fe9fb2a8
GREENOVER <humains29@gmail.com>
2020-11-13 21:05:10 +0900

    fix(Step / 그린,꼬말): 함수 순서 변경 및 자동URL 함수 수정 및 버튼 비활성화 처리 구현

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -7,21 +7,13 @@ class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var inputField: UITextField!
     @IBOutlet weak var loadIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        loadUrl()
-    }
-
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
         loadIndicator.startAnimating()
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        loadIndicator.stopAnimating()
-    }
-
     func loadUrl() {
         guard let url: URL = URL(string: "https://yagom.net") else { return }
         let request: URLRequest = URLRequest(url: url)
@@ -31,20 +23,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(request)
     }
     
-    @IBAction func touchUpChangeButton(_ sender: UIButton) {
-        let inputTextUrl: String? = inputField.text
-        guard let inputUrl: String = inputTextUrl else { return }
-
-        changeUrl(inputUrl)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadUrl()
     }
     
-    func changeUrl(_ inputUrl: String) {
-        let identifiedUrl: String = verifyUrl(inputUrl)
-        
-        guard let urlToChange: URL = URL(string: identifiedUrl) else { return }
-        let changeRequest: URLRequest = URLRequest(url: urlToChange)
-        
-        webView.load(changeRequest)
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadIndicator.stopAnimating()
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
     }
     
     @IBAction func touchUpBackButton(_ sender: UIBarButtonItem) {
@@ -63,6 +51,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
+    @IBAction func touchUpChangeButton(_ sender: UIButton) {
+        guard let inputUrl: String = inputField.text else { return }
+
+        changeUrl(inputUrl)
+    }
+    
+    func changeUrl(_ inputUrl: String) {
+        let identifiedUrl: String = verifyUrl(inputUrl)
+        
+        guard let urlToChange: URL = URL(string: identifiedUrl) else { return }
+        let changeRequest: URLRequest = URLRequest(url: urlToChange)
+        
+        webView.load(changeRequest)
+    }
+    
     func verifyUrl(_ unidentifiedUrl: String) -> String {
         var identifyUrl = unidentifiedUrl.lowercased()
         if !(identifyUrl.hasPrefix("http://") || identifyUrl.hasPrefix("https://")) {
@@ -71,6 +74,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
         return identifyUrl
     }
     
+    func autoChangeUrl(_ identifyUrl: String) -> String {
+        let autoChangedUrl = "https://" + identifyUrl
+        return autoChangedUrl
+    }
+
     /*
     func showAlertMessage() {
         let alert = UIAlertController(title: nil, message: "입력한 주소가 올바른 형태가 아닙니다.", preferredStyle: UIAlertController.Style.alert)
@@ -79,9 +87,4 @@ class ViewController: UIViewController, WKNavigationDelegate {
         present(alert, animated: true, completion: nil)
     }
     */
- 
-    func autoChangeUrl(_ identifyUrl: String) -> String {
-        let autoChangedUrl = "https://" + identifyUrl
-        return autoChangedUrl
-    }
 }
