151858aec76f878a4e6a126284d60d16286fd067
jae57 <ggapsong@naver.com>
2020-11-14 21:24:22 +0900

    step 2 first commit

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -7,15 +7,42 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
+final class ViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var URLInputField: UITextField!
+    
+    @IBAction private func moveButtonClicked(_ sender: UIButton) {
+        guard let input = URLInputField.text else { return }
+        guard let inputURL = URL(string: input) else { return }
+        let request = URLRequest(url: inputURL)
+        webView.load(request)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let myURL = URL(string: "https://www.apple.com") else { return }
-        let myRequest = URLRequest(url: myURL)
-        webView.load(myRequest)
     }
+    
+    @IBAction private func backwardClicked(_ sender: UIBarButtonItem) {
+        webView.goBack()
+        if webView.canGoBack {
+            sender.isEnabled = true
+        } else {
+            sender.isEnabled = false
+        }
+        
+    }
+    
+    @IBAction private func forwardClicked(_ sender: UIBarButtonItem) {
+        webView.goForward()
+        if webView.canGoForward {
+            sender.isEnabled = true
+        } else {
+            sender.isEnabled = false
+        }
+    }
+    
+    @IBAction private func reloadClicked(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
 }
-
